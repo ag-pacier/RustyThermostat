a21826be4bab5b868f9e6df61c234e72ceb7e351
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-08-09 13:03:23 -0400

    implemented parse_zipcode

--- src/weather.rs
+++ src/weather.rs
@@ -6,10 +6,11 @@
 
 use std::{env, fmt};
 use reqwest;
+use serde_derive::{Serialize, Deserialize};
 use serde_json;
 
 // Struct for response for the Geo stuff
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GeoLocation {
     pub zip: i32,
     pub name: String,
@@ -27,12 +28,13 @@ pub struct APIError {
     parameters: Option<Vec<String>>,
 }
 
-impl Display for APIError {
+impl fmt::Display for APIError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut display_message = String::new(self.message);
-        if (self.parameters.is_some()) {
+        let mut display_message = self.message.clone();
+        if self.parameters.is_some() {
             display_message = format!("{}, Related parameters: ", display_message);
-            for item in self.parameters.iter() {
+            let items = self.parameters.clone().unwrap();
+            for item in items.iter() {
                 display_message = format!("{}, {}", display_message, item);
             }
         }
@@ -53,51 +55,59 @@ impl Configuration {
     pub fn new() -> Configuration {
         Configuration::default()
     }
-    pub fn new_env() -> Configuration {
+    pub async fn new_env() -> Configuration {
         let mut new_config = Configuration::default();
         match env::var("WEATHER_BASE_PATH") {
-            Some(bpath) => new_config.base_path = bpath,
+            Ok(bpath) => new_config.base_path = bpath,
             _ => (), 
         }
         match env::var("WEATHER_USER_AGENT") {
-            Some(useragent) => new_config.user_agent = useragent,
+            Ok(useragent) => new_config.user_agent = Some(useragent),
             _ => (),
         }
         match env::var("WEATHER_LOCATION") {
-            Some(zip) => new_config.location = new_config.parse_zipcode(zip),
+            Ok(zip) => new_config.location = new_config.parse_zipcode(&zip).await.ok(),
             _ => (),
         }
         match env::var("WEATHER_API_KEY") {
-            Some(apikey) => new_config.api_key = apikey,
+            Ok(apikey) => new_config.api_key = Some(apikey),
             _ => (),
         }
         new_config
     }
-    pub fn api_set(self) -> bool {
-        match self.api_key {
-            Some => true,
-            _ => false,
+    pub fn api_set(&self) -> bool {
+        if self.api_key.is_some() {
+            true
+        } else {
+            false
         }
     }
-    pub async fn parse_zipcode(self, zipcode: &str) -> Result<GeoLocation, APIError> {
-        if (!self.api_set()) {
-            Err("API key not set")
-        }
-        if (!zipcode.contains(",")) {
+    pub async fn parse_zipcode(&self, zipcode: &str) -> Result<GeoLocation, APIError> {
+        let mut zip = zipcode.to_string();
+        if !zip.contains(",") {
             //If there is no comma in the provided zipcode, we probably forgot the ISO country code and I'm defaulting to the US
-            let zipcode = zipcode + ",840";
+            zip = format!("{},840", zipcode);
         }
+        let local_api_key = self.api_key.clone().unwrap_or("INVALIDKEY".to_string());
 
-        let uri = format!("{0}geo/1.0/zip?zip={1}&appid={2}", self.base_path, zipcode, &self.api_key);
+        let uri = format!("{0}geo/1.0/zip?zip={1}&appid={2}", self.base_path, zip, local_api_key);
         let mut req_builder = self.client.request(reqwest::Method::GET, uri);
-        req_builder = req_builder.header(reqwest::header::USER_AGENT, &self.user_agent);
+        req_builder = req_builder.header(reqwest::header::USER_AGENT, &self.user_agent.clone().unwrap());
 
-        let geo_response = self.client.execute(req_builder.build()).await.ok();
-        let return_status = geo_response.status();
-        let return_contents = geo_response.text().await;
+        let built_req = match req_builder.build() {
+            Ok(request) => request,
+            Err(error) => return Err(APIError{status_code: "400".to_string(), message: error.to_string(), parameters: None})
+        };
+
+        let geo_response = match self.client.execute(built_req).await {
+            Ok(resp) => resp,
+            Err(error) => return Err(APIError { status_code: error.status().unwrap().to_string(), message: error.to_string(), parameters: None })
+        };
+
+        let return_contents = geo_response.text().await.ok().unwrap();
 
         //parsing the returned JSON will either get us the GeoLocation or an APIError
-        serde_json::from_str(&return_contents).ok()
+        serde_json::from_str(&return_contents).ok().unwrap()
     }
 }
 
