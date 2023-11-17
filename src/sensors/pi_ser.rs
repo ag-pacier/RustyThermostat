41efc57438573ff10997e13bee615eb9a83ba796
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2023-11-16 22:44:17 -0500

    Created module for serial sensors
    Added notes from lessons learned from 11/16 experiments

--- src/sensors/pi_ser.rs
+++ src/sensors/pi_ser.rs
@@ -3,3 +3,9 @@
 // Use documentation from the following to help build out
 // mio-serial: https://docs.rs/mio-serial/latest/mio_serial/index.html
 // tokio-serial: https://docs.rs/tokio-serial/latest/tokio_serial/
+
+// when you start, be sure to add this: use super::*;
+
+// Note to future self
+// Keep the parsing of the sensor data here and only push them into the objects from the library when they are ready
+// Each communication type is going to have its own way of parsing the input into the correct object
