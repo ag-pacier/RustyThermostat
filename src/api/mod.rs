91403220559f790b9f22f3ff3ce73501f6e6c60f
Gage Pacier <48218237+ag-pacier@users.noreply.github.com>
2023-11-26 22:13:25 -0500

    Tested test fn for JSON sending

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,8 +1,12 @@
 use rocket::response::status;
+use rocket::serde::json::Json;
 use uuid::Uuid;
+use crate::weather::WeatherResponse;
 
 // Supporting functions to generate content for API pages goes here
-
+fn test_weather() -> WeatherResponse {
+    WeatherResponse::test_my_weather()
+}
 
 // Routes go below here for the API
 // GET routes
@@ -12,8 +16,8 @@ pub fn api_index() -> &'static str {
 }
 
 #[get("/api/weather")]
-pub fn weather() -> &'static str {
-    "Current weather"
+pub fn weather() -> Json<WeatherResponse> {
+    Json(test_weather())
 }
 
 #[get("/api/pollution")]
