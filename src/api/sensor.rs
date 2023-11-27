c412c514490fd8ca23123feec5957533fd9ba3b6
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-11-27 13:10:12 -0500

    Started work on JWTs

--- src/api/sensor.rs
+++ src/api/sensor.rs
@@ -5,7 +5,40 @@ use chrono::{Utc, NaiveDateTime};
 use std::io::Error;
 use uuid::Uuid;
 use serde_json;
+use jsonwebtoken;
 use crate::schema::{sensors, sensor_reading_history};
+
+// Structure to contain JWT info
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct JWTClaims {
+    pub iss: String,
+    pub sub: String,
+    pub groups: String,
+    pub exp: u64,
+}
+
+impl JWTClaims {
+    pub fn new(subject: Uuid) -> JWTClaims {
+        let mut new_jwt: JWTClaims = JWTClaims::default();
+        new_jwt.sub = subject.as_simple().to_string();
+        new_jwt
+    }
+}
+
+impl Default for JWTClaims {
+    fn default() -> Self {
+        let expiration: u64 = Utc::now()
+            .checked_add_signed(chrono::Duration::seconds(900))
+            .expect("Invalid timestamp")
+            .timestamp()
+            .try_into().unwrap_or(jsonwebtoken::get_current_timestamp());
+        JWTClaims {
+            iss: "Rusty Thermostat".to_string(),
+            sub: "DebugSub".to_string(),
+            groups: "WebSensor".to_string(),
+            exp: expiration }
+    }
+}
 
 // Structure to contain each web sensor
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -19,8 +52,8 @@ pub struct WebSensor {
     pub time_updated: Option<NaiveDateTime>,
     pub com_type: i32,
     pub com_last: Option<NaiveDateTime>,
-    pub current_temp_c: Option<f64>,
-    pub current_temp_f: Option<f64>,
+    pub current_temp_c: Option<f32>,
+    pub current_temp_f: Option<f32>,
     pub current_humid: Option<i32>,
     pub presence: Option<bool>,
     pub threshold_open: Option<bool>,
@@ -64,11 +97,11 @@ impl WebSensor {
         self.associated_zone = new_zone;
         self.update_time();
     }
-    pub fn set_f_temp(&mut self, f_temp: Option<f64>) -> () {
+    pub fn set_f_temp(&mut self, f_temp: Option<f32>) -> () {
         self.current_temp_f = f_temp;
         self.update_time();
     }
-    pub fn set_c_temp(&mut self, c_temp: Option<f64>) -> () {
+    pub fn set_c_temp(&mut self, c_temp: Option<f32>) -> () {
         self.current_temp_c = c_temp;
         self.update_time();
     }
