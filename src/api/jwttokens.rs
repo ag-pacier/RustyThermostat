e841ee282ac059dc430df8ea208dee258cac81e5
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-11-27 17:01:52 -0500

    Added encoding and made it generic

--- src/api/jwttokens.rs
+++ src/api/jwttokens.rs
@@ -1,4 +1,5 @@
-use jsonwebtoken;
+use jsonwebtoken::{self, Algorithm, Header, EncodingKey, encode};
+use jsonwebtoken::errors::Error;
 use serde_derive::{Serialize, Deserialize};
 use uuid::Uuid;
 use chrono::Utc;
@@ -13,24 +14,29 @@ pub struct JWTClaims {
 }
 
 impl JWTClaims {
-    pub fn new(subject: Uuid) -> JWTClaims {
+    pub fn new_sensor(subject: Uuid, sec_token: [u8; 32]) -> Result<String, Error> {
         let mut new_jwt: JWTClaims = JWTClaims::default();
         new_jwt.sub = subject.as_simple().to_string();
-        new_jwt
-    }
-}
-
-impl Default for JWTClaims {
-    fn default() -> Self {
-        let expiration: u64 = Utc::now()
+        new_jwt.groups = "WebSensor".to_string();
+        new_jwt.exp = Utc::now()
             .checked_add_signed(chrono::Duration::seconds(900))
             .expect("Invalid timestamp")
             .timestamp()
             .try_into().unwrap_or(jsonwebtoken::get_current_timestamp());
+        let header = Header::new(Algorithm::HS512);
+        encode(&header, &new_jwt, &EncodingKey::from_secret(&sec_token))
+    }
+
+
+}
+
+impl Default for JWTClaims {
+    fn default() -> Self {
+        let expiration: u64 = jsonwebtoken::get_current_timestamp();
         JWTClaims {
             iss: "Rusty Thermostat".to_string(),
             sub: "DebugSub".to_string(),
-            groups: "WebSensor".to_string(),
+            groups: "NONE".to_string(),
             exp: expiration }
     }
 }
