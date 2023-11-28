e6626fa9d65e67798bb0803401daf3230f98248a
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-11-28 08:32:18 -0500

    Completed JWT for now

--- src/api/jwttokens.rs
+++ src/api/jwttokens.rs
@@ -1,4 +1,4 @@
-use jsonwebtoken::{self, Algorithm, Header, EncodingKey, encode};
+use jsonwebtoken::{self, Algorithm, Header, EncodingKey, encode, decode, DecodingKey, Validation};
 use jsonwebtoken::errors::Error;
 use serde_derive::{Serialize, Deserialize};
 use uuid::Uuid;
@@ -27,7 +27,44 @@ impl JWTClaims {
         encode(&header, &new_jwt, &EncodingKey::from_secret(&sec_token))
     }
 
+    pub fn new_collector(subject: Uuid, sec_token: [u8; 32]) -> Result<String, Error> {
+        let mut new_jwt: JWTClaims = JWTClaims::default();
+        new_jwt.sub = subject.as_simple().to_string();
+        new_jwt.groups = "Collector".to_string();
+        new_jwt.exp = Utc::now()
+            .checked_add_signed(chrono::Duration::seconds(900))
+            .expect("Invalid timestamp")
+            .timestamp()
+            .try_into().unwrap_or(jsonwebtoken::get_current_timestamp());
+        let header = Header::new(Algorithm::HS512);
+        encode(&header, &new_jwt, &EncodingKey::from_secret(&sec_token))
+    }
 
+    pub fn new_viewer(subject: Uuid, sec_token: [u8; 32]) -> Result<String, Error> {
+        let mut new_jwt: JWTClaims = JWTClaims::default();
+        new_jwt.sub = subject.as_simple().to_string();
+        new_jwt.groups = "ViewOnly".to_string();
+        new_jwt.exp = Utc::now()
+            .checked_add_signed(chrono::Duration::seconds(900))
+            .expect("Invalid timestamp")
+            .timestamp()
+            .try_into().unwrap_or(jsonwebtoken::get_current_timestamp());
+        let header = Header::new(Algorithm::HS512);
+        encode(&header, &new_jwt, &EncodingKey::from_secret(&sec_token))
+    }
+
+    pub fn new_admin(subject: Uuid, sec_token: [u8; 32]) -> Result<String, Error> {
+        let mut new_jwt: JWTClaims = JWTClaims::default();
+        new_jwt.sub = subject.as_simple().to_string();
+        new_jwt.groups = "Super".to_string();
+        new_jwt.exp = Utc::now()
+            .checked_add_signed(chrono::Duration::seconds(900))
+            .expect("Invalid timestamp")
+            .timestamp()
+            .try_into().unwrap_or(jsonwebtoken::get_current_timestamp());
+        let header = Header::new(Algorithm::HS512);
+        encode(&header, &new_jwt, &EncodingKey::from_secret(&sec_token))
+    }
 }
 
 impl Default for JWTClaims {
@@ -39,4 +76,9 @@ impl Default for JWTClaims {
             groups: "NONE".to_string(),
             exp: expiration }
     }
+}
+
+pub fn decode_claim(token: String, sec_token: [u8; 32]) -> Result<JWTClaims, Error> {
+    let decoded_token: jsonwebtoken::TokenData<JWTClaims> = decode::<JWTClaims>(&token, &DecodingKey::from_secret(&sec_token), &Validation::default())?;
+    Ok(decoded_token.claims)
 }
