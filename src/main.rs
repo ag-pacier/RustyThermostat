be720ebb53b161b665f675c1b57dba11acb57c6b
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-11-30 13:58:44 -0500

    Added very basic config

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use config::{Config, File};
+use serde_derive::Deserialize;
+
 pub mod weather;
 pub mod schema;
 pub mod dbman;
 
 #[macro_use] extern crate rocket;
+
+#[derive(Debug, Deserialize)]
+struct AppConfiguration {
+    weather_settings: weather::Configuration,
+    db_settings: dbman::DBConfig,
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -11,5 +20,7 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
+    let application_config: Config = Config::builder().add_source(File::with_name("C:/test.toml")).build().unwrap();
+    let app_settings: AppConfiguration = application_config.try_deserialize().unwrap();
     rocket::build().mount("/", routes![index])
 }
