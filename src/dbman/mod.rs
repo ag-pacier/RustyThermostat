cf1a229ada49eddc739bab16d63c2d9815451aab
ag-pacier <48218237+ag-pacier@users.noreply.github.com>
2023-12-04 11:31:28 -0500

    Added logging to dbman

--- src/dbman/mod.rs
+++ src/dbman/mod.rs
@@ -35,6 +35,15 @@ impl DBConfig {
     /// If needed, this can accept a schema path and a port number
     /// Port will default to PostgreSQL default of 5432/TCP
     pub fn new_postgres(dbhost: String, user: String, pass: String, db: String, schema_path: Option<String>, port: Option<u32>,) -> DBConfig {
+        debug!("Creating DBConfig based on PostgreSQL");
+        debug!("Host: {}, DB: {},", &dbhost, &db);
+        trace!("User: {}, Password: {}", &user, &pass);
+        if schema_path.is_some() {
+            trace!("Schema path set to: {}", schema_path.clone().unwrap());
+        }
+        if port.is_some() {
+            debug!("Port set to: {}", port.clone().unwrap());
+        }
         DBConfig {
             protocol: "postgres://".to_string(),
             port: Some(port.unwrap_or(5432)),
@@ -56,6 +65,7 @@ impl DBConfig {
     /// Creates a DBConfig that makes assumptions based on a SQLite setup
     /// dbpath is expecting a path that starts with either the drive letter or the first folder closest to root
     pub fn new_sqlite(dbpath: String) -> DBConfig {
+        debug!("Creating DBConfig based on SQLITE");
         let mut new_db: DBConfig = DBConfig::default();
         new_db.protocol = "sqlite://".to_string();
         new_db.dbhost = format!("{}?mode=rwc", dbpath);
@@ -80,6 +90,7 @@ impl DBConfig {
             connect_string = format!("{}?currentSchema={}", connect_string, schema_choice);
         }
 
+        debug!("Connection string built: {}", &connect_string);
         connect_string
     }
 
@@ -101,32 +112,42 @@ impl DBConfig {
         let local_config: DBConfig = self.clone();
         let mut opt = ConnectOptions::new(local_config.build_connect_string());
         if let Some(con_max) = local_config.con_max {
+            debug!("Set max connections to: {}", con_max.clone());
             opt.max_connections(con_max);
         }
         if let Some(con_min) = local_config.con_min {
+            debug!("Set min connections to: {}", con_min.clone());
             opt.min_connections(con_min);
         }
         if let Some(con_time) = local_config.timeout_con {
+            debug!("Set connection timeout to: {}", con_time.clone());
             opt.connect_timeout(Duration::from_secs(con_time));
         }
         if let Some(acq_time) = local_config.timeout_acq {
+            debug!("Set acquire timeout to: {}", acq_time.clone());
             opt.acquire_timeout(Duration::from_secs(acq_time));
         }
         if let Some(idle_time) = local_config.timeout_idle {
+            debug!("Set idle timeout to: {}", idle_time.clone());
             opt.idle_timeout(Duration::from_secs(idle_time));
         }
         if let Some(lifetime) = local_config.max_life {
+            debug!("Set max lifetime to: {}", lifetime.clone());
             opt.max_lifetime(Duration::from_secs(lifetime));
         }
         if let Some(schemapath) = local_config.schema_path {
+            debug!("Set schema path to: {}", schemapath.clone());
             opt.set_schema_search_path(schemapath);
         }
         if local_config.log {
+            debug!("Set SQLX logging to true!");
             opt.sqlx_logging(true);
         } else {
+            debug!("Set SQLX logging to false!");
             opt.sqlx_logging(false);
         }
-        opt.sqlx_logging_level(local_config.log_level);
+        opt.sqlx_logging_level(local_config.log_level.clone());
+        debug!("SQLX logging level set to: {}", local_config.log_level);
 
         opt
     }
